0934696fcbc988cba93e18d6b57222134201a872
Juan Antonio Domínguez <97060625+jdomingu98@users.noreply.github.com>
2024-10-25 10:25:37 +0200

    feat(react): recreate frontend

---
+++ frontend/src/store/store.js
@@ -0,0 +1,10 @@
+// src/store/store.js
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    // Aquí irán los reducers que definas más adelante
+  },
+});
+
+export default store;
